01946aa194945e1f668289ff045d4b12aa27edc8
Jerry Qu <t-chq@microsoft.com>
2019-07-23 14:28:13 -0700

    refactoring in progress, separate player

---
+++ consts.js
@@ -0,0 +1,10 @@
+
+
+const charHeight = 30;
+const platformHeight = 10;
+const charWidth = 20;
+const meteorRad = 10;
+const platformWidth = canvas.width*1/2;
+const platformX = (canvas.width-platformWidth)/2; //left bound of the platform
+const charSpeed = 2;//default movement speed of the player per frame
+const meteorSpeed = 2; //default fall speed for meteors per frame
