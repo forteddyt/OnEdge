09c1ce7bf42b621e14215b32ee28035fe92d8ed3
t-thtodo <t-thtodo@microsoft.com>
2019-07-23 14:05:26 -0700

    added space

---
+++ space.js
@@ -0,0 +1,34 @@
+function Space(canvasCtx){
+    // Singleton
+    if(Space.instance_){
+        return Space.instance_;
+    }
+
+    this.canvasCtx = canvasCtx
+    this.lastSpawnTime = 0;
+
+    this.meteors = [];
+
+    this.instance_ = this;
+    this.init()
+}
+
+Space.prototype = {
+    init: function(){
+        this.time = 0;
+        this.update()
+    },
+    update: function(runningTime){
+        if(runningTime - this.lastSpawnTime > 750) {
+            this.spawnMeteor()
+            this.lastSpawnTime = runningTime;
+        }
+
+        this.meteors.forEach(function(meteor){
+            meteor.update();
+        })
+    },
+    spawnMeteor: function(){
+        this.meteors.push(new Meteor(this.canvasCtx))
+    }
+}
