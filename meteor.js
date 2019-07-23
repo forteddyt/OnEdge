36b33c73480affb6b5f65a5a06fecbe3e4f4b823
t-thtodo <t-thtodo@microsoft.com>
2019-07-23 14:05:36 -0700

    added meteor objects

---
+++ meteor.js
@@ -0,0 +1,43 @@
+MeteorImg = new Image();
+MeteorImg.src = 'images/asteroid-png-8-bit.png'
+meteors = new Array()
+
+function Meteor(canvasCtx){
+    this.canvasCtx = canvasCtx
+    this.xPos = 0;
+    this.yPos = 0;
+    this.dX = 0;
+    this.dY = 0;
+    this.imgSprite = null;
+
+    this.init()
+}
+
+Meteor.prototype = {
+    init: function() {
+        this.imgSprite = MeteorImg;
+        this.xPos = Math.floor(Math.random() * (this.canvasCtx.canvas.width - 10) + 5);
+        this.yPos = -this.imgSprite.height - 10; // Meteor should start off of the screen
+        this.dX = Math.random() * 1.5 - .75;
+        this.dY = Math.random() * 3;
+        
+        this.draw();
+        meteors.push(this)
+    },
+    draw: function() {
+        if(inCtxBounds(this.imgSprite, this.xPos, this.yPos, this.canvasCtx)){
+            ctx.drawImage(this.imgSprite, this.xPos, this.yPos)
+
+            this.xPos += this.dX
+            this.yPos += this.dY
+        }
+    },
+    update: function() {
+        this.draw()
+    }
+}
+
+function inCtxBounds(img, x, y, ctx){
+    return x > 0 - img.width && x < ctx.canvas.width + img.width
+        && y < ctx.canvas.height + img.height;
+}
