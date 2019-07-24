80dcdcab28faa27f7c64eded7a32fcfaae1dfe1e
Jerry Qu <t-chq@microsoft.com>
2019-07-24 15:38:10 -0700

    fix score when game over

--- score.js
+++ score.js
@@ -26,18 +26,22 @@ Score.prototype = {
         this.canvasCtx.font = "20px Monospace";
         this.canvasCtx.fillStyle = "black";
         this.canvasCtx.textAlign = "left";
-        if (this.score == 100 || this.score == 250 || this.score == 500 || this.score == 750 || this.score == 1000 || this.score == 1500 || (this.score % 1000 == 0 && this.score > 0)) {
-            this.scoreFlashDuration = this.SCORE_FLASH_FRAMES;
-            this.scoreFlashValue = this.score;
-        }
-    
-        if (this.scoreFlashDuration > 0) {
-            this.scoreFlashDuration--;
-            if (Math.floor(this.scoreFlashDuration / this.SCORE_FLASH_SPEED) % 2 == 1) {
-                this.canvasCtx.fillText(this.scoreFlashValue, 10, 20);
-            }
-        } else {
+        if (this.Game.gameOver) {
             this.canvasCtx.fillText(this.score, 10, 20);
+        } else {
+            if (this.score == 100 || this.score == 250 || this.score == 500 || this.score == 750 || this.score == 1000 || this.score == 1500 || (this.score % 1000 == 0 && this.score > 0)) {
+                this.scoreFlashDuration = this.SCORE_FLASH_FRAMES;
+                this.scoreFlashValue = this.score;
+            }
+        
+            if (this.scoreFlashDuration > 0) {
+                this.scoreFlashDuration--;
+                if (Math.floor(this.scoreFlashDuration / this.SCORE_FLASH_SPEED) % 2 == 1) {
+                    this.canvasCtx.fillText(this.scoreFlashValue, 10, 20);
+                }
+            } else {
+                this.canvasCtx.fillText(this.score, 10, 20);
+            }
         }
     },
     update: function(frameCount){
