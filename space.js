b86a01dc4240a9f4c66a8c7bbfdc3a66b33ff672
Jerry Qu <t-chq@microsoft.com>
2019-07-24 12:06:57 -0700

    add stars to game

--- space.js
+++ space.js
@@ -6,6 +6,7 @@ function Space(game){
     this.Game = game
     this.canvasCtx = null
     this.meteors = [];
+    this.stars = [];
     this.previousMeteorX = 0;
 
     this.instance_ = this;
@@ -23,8 +24,16 @@ Space.prototype = {
             this.spawnMeteor(frameCount);
         }
 
+        if(this.shouldSpawnStar(frameCount)) {
+            this.spawnStar(frameCount);
+        }
+
         this.meteors.forEach(function(meteor){
             meteor.update(frameCount);
+        })
+
+        this.stars.forEach(function(star){
+            star.update(frameCount);
         })
     },
 
@@ -38,6 +47,15 @@ Space.prototype = {
         //spawn rate of meteors increases asymptotically with time
         var invFreq = Math.ceil(10 + 50/((frameCount + 1500)/1500));
         if (frameCount % invFreq == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    shouldSpawnStar: function(frameCount){
+        //spawn rate of meteors increases asymptotically with time
+        if (frameCount % 500 == 0) {
             return true;
         } else {
             return false;
@@ -78,10 +96,37 @@ Space.prototype = {
         this.previousMeteorX = this.meteors[this.meteors.length-1].xPos;
     },
 
+    spawnStar : function(frameCount){
+        var rand = Math.random();
+        if (frameCount < 1000) {
+            this.stars.push(new Star(this.canvasCtx, this.instance_, 0));
+        } else if (frameCount < 3000) {
+            if (rand < 0.5) {
+                this.stars.push(new Star(this.canvasCtx, this.instance_, 0));
+            } else {
+                this.stars.push(new Star(this.canvasCtx, this.instance_, 1));
+            }
+        } else {
+            if (rand < 0.3) {
+                this.stars.push(new Star(this.canvasCtx, this.instance_, 0));
+            } else {
+                this.stars.push(new Star(this.canvasCtx, this.instance_, 1));
+            }
+        }
+    },
+
     removeMeteor: function(targetMeteor){
         for (var i = 0; i < meteors.length; i++) {
             if (meteors[i] === targetMeteor) {
                 meteors.splice(i, 1);
+            }
+        } 
+    },
+
+    removeStar: function(targetStar){
+        for (var i = 0; i < stars.length; i++) {
+            if (stars[i] === targetStar) {
+                stars.splice(i, 1);
             }
         }
     }
---
+++ star.js
@@ -0,0 +1,83 @@
+StarImg = new Image();
+StarImg.src = 'images/star.png'
+stars = new Array()
+
+function Star(canvasCtx, space, type, prevX){
+    this.canvasCtx = canvasCtx
+    this.xPos = 0;
+    this.yPos = 0;
+    this.dX = 0;
+    this.dY = 0;
+    this.type = type;
+    this.prevX = prevX;
+
+    this.nImage = 0;
+    this.imgWidth = 40;
+    this.imgHeight = 60;
+
+    this.frameCount = 0;
+    this.frameLoopCycle = 15;
+    this.space = space;
+    this.init()
+}
+
+Star.prototype = {
+    init: function() {
+        this.imgSprite = StarImg;
+
+        this.xPos = Math.floor(Math.random() * (this.canvasCtx.canvas.width*3/5) + this.canvasCtx.canvas.width/5);
+
+        this.yPos = -this.imgSprite.height - 10; // star should start off of the screen
+        if (this.type == 0) {
+            this.dX = 0;
+            this.dY = 1.5;
+        } else if (this.type == 1) {
+            this.dX = 0;
+            this.dY = 3;
+        } else if (this.type == 2) {
+            this.dX = 0;
+            this.dY = 4.5;
+        } else if (this.type == 3) {
+            console.log("this is intense!");
+            this.dX = Math.random() > 0.5 ? -1.5 : 1.5;
+            this.dY = 3;
+        }
+        //this.dX = Math.random() * 1.5 - .75;
+        //this.dY = Math.random() * 3;
+        this.radius = this.imgWidth/4;
+        this.draw();
+        stars.push(this)
+    },
+    
+    draw: function() {
+        // this.canvasCtx.beginPath();
+        // this.canvasCtx.arc(this.xPos, this.yPos+this.imgHeight-(0.8*this.imgWidth), 0.8*(this.imgWidth/2), 0, 2 * Math.PI);
+        // this.canvasCtxfillStyle = "#0095DD";
+        // this.canvasCtx.fill();
+        // this.canvasCtx.closePath();
+        this.canvasCtx.drawImage(StarImg, this.nImage * this.imgWidth, 0, this.imgWidth, this.imgHeight, this.xPos, this.yPos, this.imgWidth, this.imgHeight);
+
+        if (this.frameCount % this.frameLoopCycle == 0) {
+            this.nImage++;
+        }
+        if (this.nImage > 3) {
+            this.nImage = 0;
+        }
+    },
+
+    update: function(frameCount) {
+        this.frameCount = frameCount
+        if(inCtxBounds(this.imgSprite, this.xPos, this.yPos, this.canvasCtx)){
+            this.xPos += this.dX
+            this.yPos += this.dY
+            this.draw()
+        } else {
+            this.space.removeStar(this);
+        }
+    }
+}
+
+function inCtxBounds(img, x, y, ctx){
+    return x > 0 - img.width && x < ctx.canvas.width + img.width
+        && y < ctx.canvas.height + img.height;
+}
